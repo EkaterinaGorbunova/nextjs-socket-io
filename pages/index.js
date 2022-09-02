31c35f70a8db5fb51f8e9d8368730e410010b4e9
EkaterinaGorbunova <ekaterina.nalitova@gmail.com>
2022-09-01 22:40:56 -0700

    added title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+            Next.js + Socket.io!
           </a>
         </h1>
 
